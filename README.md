73a1caad1a5d16c85c5760129b7abf0dc79397bc
Rosa Voogd <rosavoogd@gmail.com>
2020-06-11 16:37:03 +0200

    index.js cleaned

--- README.md
+++ README.md
@@ -37,3 +37,125 @@ npm install
 
 
 https://rosavoogd.github.io/datingapp/
+
+
+
+## Probeerseltjes:
+
+```js
+
+
+function about(req, res) {
+    res.send("Hello About");
+}
+
+app.get("/about", about);
+
+function animal(req, res){
+    res.render('list', {data: data});
+}
+
+app.get("/animal", animal);
+
+function files(req, res){
+    console.log(req.params);
+    res.send('files');
+}
+
+app.get("/files/:type", files);
+
+
+/* FORM ASSIGNMENT */
+function form(req, res){
+    res.render('settings');
+}
+
+app.get("/settings", form);
+
+function settings(req, res){
+    var id = slug (req.body.species).toLowerCase();
+    
+    data.push({
+        id: id,
+        name: req.body.name,
+        species: req.body.species,
+        bio: req.body.bio,
+        age: req.body.age,
+        environment: req.body.environment,
+
+        img: req.body.img
+    });
+
+    res.redirect('/' + name);
+}
+
+app.post("/", settings);
+
+// MONGDODB FIND
+function database(req, res, next){
+    db.collection('data').find().toArray(done)
+
+    function done(err, data){
+        if (err){
+            next (err)
+        }
+        else{
+            res.render('list', {data: data})
+        }
+    }
+}
+
+//MONGODB UPDATE
+function change(req, res, next){
+    db.collection('data').updateOne({
+        _id: ObjectID(req.body.description)
+    }, done)
+
+    function done(err, data){
+        if (err){
+            next (err)
+        }
+        else{
+            res.redirect('/' + data.insertedID)
+        }
+    }
+
+}
+
+//MONGODB ADDANIMAL
+function addanimal(req, res, next){
+    db.collection('data').insertOne({
+        name: req.body.name,
+        species: req.body.species,
+        bio: req.body.bio,
+        age: req.body.age
+    },done)
+    function done(err, data){
+        if (err){
+            next(err)
+        } else{
+            res.redirect('/' + data.insertedID)
+        }
+    }
+}
+
+//SESSIONS SET
+function signup(req, res, next){
+
+    function onhash(hash){
+
+
+            function oninstert(err){
+                if (err){
+                next (err)
+            } else {
+            req.session.user = {username: username}
+            res.redirect('/')
+            }
+        }
+    }
+}
+
+
+const counter = 0;
+```
--- index.js
+++ index.js
@@ -1,194 +1,156 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const slug = require("slug");
+require('dotenv').config();
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var slug = require("slug");
+var session = require('express-session');
+var mongo = require("mongodb");
+
 const port = 5500;
-const session = require('express-session');
 
-// //MONGODB
-// var mongo = require("mongodb");
-// require(dotenv).config();
+/* DATABASE 
+================================================================================================== */
+var db = null;
+var url = "mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT;
 
-// var db = null;
-// var url = "mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT;
+mongo.MongoClient.connect(url, function (err, client){
+    if (err) throw err
+    db = client.db(process.env.DB_NAME);
+})
 
-// mongo.MongoClient.connect(url, function (err, client){
-//     if (err) throw err
-//     db = client.db(process.env.DB_NAME);
-// })
-
-app.use(session({
-  'secret': 'SESSIONS_SECRET'
-}))
-
-// set templating engine
-app.set ("view engine", "ejs");
-app.set ("views", "view");
-
-// add bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
-// add static files 
-app.use(express.static('static-website'))
-app.use('/datingapp', express.static('static-website'))
-
-function home (req, res) {
-    res.send ("Hello World!");
-}
-
-app.get("/", home);
-
-function about(req, res) {
-    res.send("Hello About");
-}
-
-app.get("/about", about);
-
-function animal(req, res){
-    res.render('list', {data: data});
-}
-
-app.get("/animal", animal);
-
-function files(req, res){
-    console.log(req.params);
-    res.send('files');
-}
-
-app.get("/files/:type", files);
-
-
-/* FORM ASSIGNMENT */
-function form(req, res){
-    res.render('settings');
-}
-
-app.get("/settings", form);
-
-function settings(req, res){
-    var id = slug (req.body.species).toLowerCase();
-    
-    data.push({
-        id: id,
-        name: req.body.name,
-        species: req.body.species,
-        bio: req.body.bio,
-        age: req.body.age,
-        environment: req.body.environment,
-
-        img: req.body.img
+/* EXPRESS SETUP
+================================================================================================== */
+express()
+    .set("view engine", "ejs")
+    .set("views", "view")
+    .use(session({
+        'secret': 'SESSIONS_SECRET'
+    }))
+    .use(bodyParser.urlencoded({extended: true}))
+    .use(express.static('static-website'))
+    .use('/datingapp', express.static('static-website'))
+    .get('/', checkAuth, getHome)
+    .get('/login', getLogin)
+    .post('/login', postLogin)
+    .get('/register', getRegister)
+    .post('/register', postRegister)
+    .listen(port, function () {
+        console.log("The server is running!");
     });
 
-    res.redirect('/' + name);
-}
+/* MIDDLEWARE
+================================================================================================== */
+function checkAuth(req, res, next) {
+    // Check of de gebruiker bestaat in de sessie
+    var loggedIn = true;
 
-app.post("/", settings);
-
-// MONGDODB FIND
-function database(req, res, next){
-    db.collection('data').find().toArray(done)
-
-    function done(err, data){
-        if (err){
-            next (err)
-        }
-        else{
-            res.render('list', {data: data})
-        }
-    }
-}
-
-//MONGODB UPDATE
-function change(req, res, next){
-    db.collection('data').updateOne({
-        _id: ObjectID(req.body.description)
-    }, done)
-
-    function done(err, data){
-        if (err){
-            next (err)
-        }
-        else{
-            res.redirect('/' + data.insertedID)
-        }
+    if (loggedIn) {
+        return next();
     }
 
+    return res.redirect('/login');
 }
 
-//MONGODB ADDANIMAL
-function addanimal(req, res, next){
-    db.collection('data').insertOne({
-        name: req.body.name,
-        species: req.body.species,
-        bio: req.body.bio,
-        age: req.body.age
-    },done)
-    function done(err, data){
-        if (err){
-            next(err)
-        } else{
-            res.redirect('/' + data.insertedID)
-        }
-    }
+/* HOMEPAGE 
+================================================================================================== */
+function getHome(req, res) {
+    /*
+        1. Haal de gebruiker op uit de sessie
+        2. Haal de dieren op uit de DB
+        3. Filter de dieren (array.filter())
+        4. Stuur dieren in res.render
+    */
+    var dieren = [];
+    res.render('index', { data: dieren });
 }
 
-//SESSIONS SET
-function signup(req, res, next){
+/* LOGIN
+================================================================================================== */
+function getLogin(req, res) {
+    res.render('login');
+}
 
-    function onhash(hash){
+function postLogin(req, res) {
+    /**
+     * 1. Check of er een email in req.body bestaat (zo niet error)
+     * 2. Haal gebruiker op uit de database op basis van email - find mongo 
+     * 3. Check of gebruiker bestaat, zo niet error
+     * 4. Sla gebruiker op in sessie
+     */
+    res.redirect('/');
+}
 
+/* REGISTER
+================================================================================================== */
+function getRegister(req, res) {
+    res.render('register');
+}
 
-            function oninstert(err){
-                if (err){
-                next (err)
-            } else {
-            req.session.user = {username: username}
-            res.redirect('/')
-            }
-        }
-    }
+function postRegister(req, res) {
+    /**
+     * 1. Haal gebruikers uit DB waar email == req.body.email
+     * 2. Kijk of die gebruiker bestaat, zo niet error
+     * 3. Sla de gebruiker op in de DB
+     * 4. Log de gebruiker in, (sla op in sessie)
+     */
+
+    res.redirect('/');
+}
+
+/* MESSAGES
+================================================================================================== */
+function getMessages(req, res) {
+    res.render('messages');
+
+    /**
+     * 1. Check of de gebruikers sessie al messages heeft in de DB
+     * 2. Haal messages uit de DB van de gebruikers sessie
+     * 3. Sorteer de messages op datum, recent eerst
+     */
+}
+
+/* MESSAGES:ID
+================================================================================================== */
+function getMessagesId(req, res) {
+    res.render('messagesId');
+}
+
+function postMessagesId(req, res) {
+   
+    /**
+    * 1. Maak een typveld voor het bericht 
+    * 2. Check of er een bericht getyped is.
+    * 3. Is er een bericht getyed en wordt er op send gedrukt, verstuur bericht 
+    * 
+    * */
 }
 
 
-const counter = 0;
+/* APPOINTMENTS
+================================================================================================== */
+function getAppointments(req, res) {
+    res.render('appointments');
 
-/**************************************/
-/* THE ANIMALS IN AN ARRAY AS OBJECTS */
-/**************************************/
-const data = [
-    {
-        name: "Bob",
-        species:"Cat",
-        bio: "Tabby Cat",
-        age: 3,
-        environment:["Apartment, Garden, Balcony, Farm"],
-        money: 100,
-        src: "static-website/img/bob.jpeg"
-        
-    },
-    {
-        name: "Dolly",
-        species: "Dog",
-        bio: "Labrador",
-        age: 2,
-        environment:["Garden, Balcony, Farm"],
-        money: 200,
-        src: "static-website/img/dolly.jpg"
-    },
-    {
-        name: "Pip",
-        species: "Bird",
-        bio: "Cockatoo",
-        age: 1,
-        environment:["Apartment, Garden, Balcony, Farm"],
-        money: 50,
-        src: "static-website/img/bob.jpeg"
+    /**
+     * 1. Check of er appointments voor de gebruiker aanwezig zijn in de database
+     * 2. Haal appointments voor de gebruiker uit de database
+     * 3. Weergeef de appointments en sorteer op dichtsbijzijnde datum
+     */
+}
+
+
+
+/* PROFILE
+================================================================================================== */
+function getProfile(req,res) {
+    res.render('profile');
+
+    function postProfile(req,res) {
+
+        /**
+         * 1. check of het profiel met de preferences al een keer is ingevuld
+         * 2. Als het al is ingevuld, toon de preferences, zo niet, toon een leeg form
+         * 3. Als er aanpassingen gemaakt worden en de gebruiker klikt op save, onthoud deze sessie
+         */
     }
-];
-
-
-app.listen(port, function() {
-    console.log("The server is running!");
-});
-
-//server is geschreven, in de terminal doe je node index.js en hij runt
-//nu kan ik npm start doen om de server en nodemon te starten 
+}
