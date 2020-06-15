de10df7f79f3d299f5d53d319d42d4a547a93f2e
Rosa Voogd <rosavoogd@gmail.com>
2020-06-15 15:56:14 +0200

    heroku

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const port = 5500;
 
 /* DATABASE 
 ================================================================================================== */
+
 var db = null;
 var url = process.env.DB_HOST 
 console.log(url);
@@ -20,7 +21,6 @@ mongo.MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client
     if (err) throw err
     db = client.db(process.env.DB_NAME);
 })
-
 
 
 /* EXPRESS SETUP
@@ -34,19 +34,20 @@ express()
     .use(bodyParser.urlencoded({extended: true}))
     .use(express.static('static'))
     .use('/datingapp', express.static('static'))
+    
     .get('/', getHome)
 
-    .get('/login', getLogin)
-    .post('/login', postLogin)
+    // .get('/login', getLogin)
+    // .post('/login', postLogin)
 
-    .get('/register', getRegister)
-    .post('/register', postRegister)
+    // .get('/register', getRegister)
+    // .post('/register', postRegister)
 
-    .get('/messages', getMessages)
-    .get('/messagesId', getMessagesId)
-    .post('/messagesId', postMessagesId)
+    // .get('/messages', getMessages)
+    // .get('/messagesId', getMessagesId)
+    // .post('/messagesId', postMessagesId)
 
-    .get('/appointments', getAppointments)
+    // .get('/appointments', getAppointments)
 
     .get('/profile', getProfile)
     .post('/profile', postProfile)
@@ -54,6 +55,7 @@ express()
     
     .listen(port, function () {
         console.log("The server is running!");
+        'http://localhost:${PORT}'
     });
 
 /* SESSIONS
@@ -87,6 +89,7 @@ function getHome(req, res, next) {
         next(err)
       } else {
         console.log(data)
+        res.render('index.ejs');
       }
     }
     
@@ -94,33 +97,33 @@ function getHome(req, res, next) {
 
 /* LOGIN
 ================================================================================================== */
-function getLogin(req, res) {
-    res.render('/login');
-}
-    function postLogin(req, res) {
-    /**
-     * 1. Check of er een email in req.body bestaat (zo niet error)
-     * 2. Haal gebruiker op uit de database op basis van email - find mongo 
-     * 3. Check of gebruiker bestaat, zo niet error
-     * 4. Sla gebruiker op in sessie
-     */
-    res.redirect('/');
-}
+// function getLogin(req, res) {
+//     res.render('/login');
+// }
+//     function postLogin(req, res) {
+//     /**
+//      * 1. Check of er een email in req.body bestaat (zo niet error)
+//      * 2. Haal gebruiker op uit de database op basis van email - find mongo 
+//      * 3. Check of gebruiker bestaat, zo niet error
+//      * 4. Sla gebruiker op in sessie
+//      */
+//     res.redirect('/');
+// }
 
 /* REGISTER
 ================================================================================================== */
-function getRegister(req, res) {
-    res.render('/register');
-}
-    function postRegister(req, res) {
-    /**
-     * 1. Haal gebruikers uit DB waar email == req.body.email
-     * 2. Kijk of die gebruiker bestaat, zo niet error
-     * 3. Sla de gebruiker op in de DB, en laat het form profile invullen
-     * 4. Log de gebruiker in, (sla op in sessie)
-     */
-    res.redirect('/');
-}
+// function getRegister(req, res) {
+//     res.render('/register');
+// }
+//     function postRegister(req, res) {
+//     /**
+//      * 1. Haal gebruikers uit DB waar email == req.body.email
+//      * 2. Kijk of die gebruiker bestaat, zo niet error
+//      * 3. Sla de gebruiker op in de DB, en laat het form profile invullen
+//      * 4. Log de gebruiker in, (sla op in sessie)
+//      */
+//     res.redirect('/');
+// }
 
 /* MESSAGES
 ================================================================================================== */
@@ -134,36 +137,39 @@ function getMessages(req, res) {
 
 /* MESSAGES:ID
 ================================================================================================== */
-function getMessagesId(req, res) {
-    res.render('/messagesId');
-}
-    function postMessagesId(req, res) {
-    /**
-    * 1. De gebruiker verstuurd het bericht
-    * 2. Bestaat de huidige chat
-    * 3. Sla bericht op in huidige chat
-    * 4. Ververs chat pagina (redirect naar /message/ + huidig ID
-    * */
-}
+// function getMessagesId(req, res) {
+//     res.render('/messagesId');
+// }
+//     function postMessagesId(req, res) {
+//     /**
+//     * 1. De gebruiker verstuurd het bericht
+//     * 2. Bestaat de huidige chat
+//     * 3. Sla bericht op in huidige chat
+//     * 4. Ververs chat pagina (redirect naar /message/ + huidig ID
+//     * */
+// }
 
 
 /* APPOINTMENTS
 ================================================================================================== */
-function getAppointments(req, res) {
-    res.render('/appointments');
-    /**
-     * 1. Haal appointments voor de gebruiker uit de database, zijn er geen appointments -> "no appointments yet"
-     * 2. Weergeef de appointments en sorteer op dichtsbijzijnde datum
-     */
-}
+// function getAppointments(req, res) {
+//     res.render('/appointments');
+//     /**
+//      * 1. Haal appointments voor de gebruiker uit de database, zijn er geen appointments -> "no appointments yet"
+//      * 2. Weergeef de appointments en sorteer op dichtsbijzijnde datum
+//      */
+// }
 
 
 /* PROFILE
 ================================================================================================== */
 function getProfile(req, res) {
-    res.render('/profile');
+    res.render('profile.ejs');
 }
     function postProfile(req,res) {
+        //helped by NathanNeelis 
+        const filterAnimal1 = req.body.cat;
+        req.session.filter = filterAnimal1;
         /**
          * 1. check of het profiel met de preferences al een keer is ingevuld
          * 2. Als het al is ingevuld, toon de preferences, zo niet, toon een leeg form
@@ -171,11 +177,18 @@ function getProfile(req, res) {
          * 4. Ververs profiel pagina (redirect naar /profile/)
          */
 
-         data.push({
-             id: id,
-             species: req.body.species,
-             age: req.body.age
-         })
+         const done = (filterAnimal) => {
+             if (dataFilter) {
+                 console.log(req.session.filter);
+                 res.render('profile.ejs', {
+                     dataFilter: req.session.filter,
+                 });
+             } else {
+                 res.render('profile.ejs', {   
+                 });
+             }
+         };
+         done(filterAnimal1);
 
-         res.redirect('/profile');
+         res.redirect('profile.ejs');
     }
